ca47080a520885a91a29cad8681f49c2389ab155
Unknown <ajpuchalski@gmail.com>
2018-07-29 11:44:26 -0500

    form posted to page July 29

--- client/src/components/QuizForm.js
+++ client/src/components/QuizForm.js
@@ -1,72 +1,102 @@
 import React, {Component} from "react";
 import "./QuizForm.css";
-import "./AddChoices.js";
+import { button } from "reactstrap";
 
 
-class QuizForm extends Component{
-  state = {
-    title: '',
-    question: '',
-    answer: '',
-    choices: '',
-    inputLinkClicked: false,
-
-  }
-
-
-
-  change = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({
+const QuizArray = [
+  {
       title: '',
       question: '',
-      answer:'',
-      choices:''
-   });
-   this.props.onChange({
-     title: '',
-     question: '',
-     answer:'',
-     choices:''
-   });
- };
+      answer: '',
+      choiceA: '',
+      choiceB: '',
+      choiceC: '',
+      choiceD: '',
+    }
+  ];
 
- onClick = e => {
-   e.preventDefault();
-   this.props.onClick(this.state);
-   this.setState({
-     inputLinkClicked: true
-   })
- }
+  class QuizForm extends Component {
 
+    constructor() {
+      super();
+      this.state = {
+        Quiz: QuizArray
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  render(){
-    return(
-      <form  className="form">
-        <input name="title" placeholder="quiz title" value={this.state.title} onChange={e => this.change(e)}/><br/>
-        <input name="question" placeholder="question" value={this.state.question} onChange={e => this.change(e)}/><br/>
-        <input name="answer" placeholder="answer" value={this.state.answer} onChange={e => this.change(e)}/><br/>
-        <input name="choices" placeholder="choices" value={this.state.choices} onChange={e => this.change(e)}/>
-        <br/>
+    handleSubmit(e) {
+      e.preventDefault();
+      const
+      { Quiz } = this.state,
+      title = this.refs.title.value,
+      question = this.refs.question.value,
+      answer = this.refs.answer.value,
+      choiceA = this.refs.choiceA.value,
+      choiceB = this.refs.choiceB.value,
+      choiceC = this.refs.choiceC.value,
+      choiceD = this.refs.choiceD.value
+      this.setState({
+        Quiz: [...Quiz, {
+          title,
+          question,
+          answer,
+          choiceA,
+          choiceB,
+          choiceC,
+          choiceD
+        }]
+      }, () => {
+        this.refs.title.value = '';
+        this.refs.question.value = '';
+        this.refs.answer.value = '';
+        this.refs.choiceA.value = '';
+        this.refs.choiceB.value = '';
+        this.refs.choiceC.value = '';
+        this.refs.choiceD.value = '';
+      });
+    }
 
+    render() {
+      const { Quiz } = this.state;
+      console.log('message',this.state.Quiz);
+      return (
+        <div>
+          <div id="titleForm">
+          <h2>Create Quiz</h2>
+          <input type="text" ref="title" placeholder="title" /><br/>
+        </div>
+          <div className="grid">
+   
+          <form onSubmit={this.handleSubmit}>
+            <div className="col-2-3">
+            <h1>Question</h1>
 
-       <input label=""type="text" id="B" name="B"/>
-       <button type="button" className="make-button-link" onClick={this.handleAddSecondInput}>
-              Add
-        </button>
-        <button onClick={e => this.onSubmit(e) }>Submit</button><br/>
-      </form>
+            <input type="text" ref="question" placeholder="question" /><br/>
+            <input type="text" ref="answer" placeholder="answer" /><br/>
+            <input type="text" ref="choiceA" placeholder="choiceA" /><br/>
+            <input type="text" ref="choiceB" placeholder="choiceB" /><br/>
+            <input type="text" ref="choiceC" placeholder="choiceC" /><br/>
+            <input type="text" ref="choiceD" placeholder="choiceD" /><br/>
 
-    );
+            <button block type="submit">Add Question to Quiz</button>
+          </div>  
+          </form>
+          <div className="col-1-3">
+          <h2>Existing Questions</h2>
+          <ul>
+            {Quiz.map((Quiz) =>
+            <li> {` ${Quiz.question} ${Quiz.answer} ${Quiz.choiceA} ${Quiz.choiceB} ${Quiz.choiceC} ${Quiz.choiceD} `}</li>
+
+            )}
+          </ul>
+          </div>
+          </div>
+          
+        
+        </div>
+      )
+    }
   }
-}
-
 
 export default QuizForm;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './views/Home';
+import Home from './views/App';
 import Login from './views/Login';
 import MakeQuiz from './views/MakeQuiz';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route} from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Index = () => (
   <Router>
     <div>
