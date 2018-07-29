46343bef5de53e7c3512d4e263f3ca6f2e602a0e
Unknown <ajpuchalski@gmail.com>
2018-07-29 12:14:46 -0500

    navbar commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,9 +5,9 @@ import "./Navbar.css";
 const Navbar = ({title}) => (
   <div>
   <ul className="Navbar">
-    <li><a href="/">  Home</a></li>
-    <li><a href="/Login">Login</a></li>
-    <li><a href="/MakeQuiz">Make a Quiz</a></li>
+    <li><a href="/"><i id="gly" class="fas fa-home"></i>  </a></li>
+    <li><a href="/Login"><i id="gly" class="fas fa-user-astronaut"></i></a></li>
+    <li><a href="/MakeQuiz"><i id="gly" class="fas fa-pencil-alt"></i></a></li>
   </ul>
   <h1 className="Title">{title}</h1>
   
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 const Index = () => (
   <Router>
     <div>
