952d966fe95d674ede0148ece4b6aa8b550fc590
Joshua Owen <jowen94o@gmail.com>
2018-08-07 21:31:17 +0200

    Quiz page is rendering all quiz questions

--- client/src/components/DisplayQuiz.js
+++ client/src/components/DisplayQuiz.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import _ from 'lodash'
 import "./DisplayQuiz.css";
 
 
-const DisplayQuiz = () => (
-    <div className="quizContainer">
-    <div className="Qname">Relevant Questions{ /*Title*/ }</div>
-    <form>
-    <div className="Question"> 1. What are we doing? { /*Question*/ }</div>
-      <div>
-        <div className="options"><label>Eating lunch<input className="opt1" value="" type="radio" /*checked={this.state.selectedOption === 'opt1'}*//></label></div><br/>
-        <div className="options"><label>Building a React App<input className="opt2" value="" type="radio"/></label></div><br/>
-        <div className="options"><label>Knitting Mittens<input className="opt3" value="" type="radio"/></label></div><br/>
-        <div className="options"><label>Building an igloo<input className="opt4" value="" type="radio"/></label></div><br/>
-      </div>
-    </form>
-    </div>
-)
+class DisplayQuiz extends Component {
+state = {}
+
+ componentDidMount(){
+   axios.get(`/api/quizs/${window.location.search.slice(4)}`).then((res) => {
+     console.log(res);
+     this.setState({
+       title: res.data[0].title,
+       questions: res.data[0].questions
+     })
+   }).catch((err) => {
+     console.log(err)
+   })
+ }
+
+
+
+ render() {
+   console.log(this.state.title);
+   console.log(this.state.questions);
+
+   return(
+     <span>
+       {(this.state.questions) && (
+         <div className="quizContainer">
+           <div className="Qname">Quiz: {(this.state.title) ? this.state.title : 'no data'}</div>
+           {this.state.questions.map((question, index) => (
+             <div key={index}>
+               {question.map((q, i) => (
+                 <div key={i}>
+                   <p className="Question">{q.question}?</p>
+                   <ul>
+                    <div className="options"><label>{q.opt1}<input className="opt1" value="" type="radio" /*checked={this.state.selectedOption === 'opt1'}*//></label></div><br/>
+                    <div className="options"><label>{q.opt1}<input className="opt2" value="" type="radio"/></label></div><br/>
+                    <div className="options"><label>{q.opt1}<input className="opt3" value="" type="radio"/></label></div><br/>
+                    <div className="options"><label>{q.opt1}<input className="opt4" value="" type="radio"/></label></div><br/>
+                   </ul>
+                 </div>
+               ))}
+             </div>
+           ))}
+         </div>
+       )}
+     </span>
+   );
+ }
+
+
+}
+
 
 
 export default DisplayQuiz;
--- client/src/views/App.js
+++ client/src/views/App.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import QuizDisplay from '../components/QuizDisplay';
 
 
+
 import './Home.css';
 
 
